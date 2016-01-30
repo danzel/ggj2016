0c32eac5b01188c2c5b7a807c743ae08c56c5164
danzel <dave@ironshod.co.nz>
2016-01-30 17:45:56 +1300

    Have lambs spawn as the game goes

--- app/entry.ts
+++ app/entry.ts
@@ -10,10 +10,10 @@ import Ui = require('./ui');
 class AppEntry {
 	game: Phaser.Game;
 	ui: Ui;
-	
+
 	players: Array<Player> = [];
 	sacrificePits: Array<SacrificePit> = [];
-	
+
 	swapButtonDown: boolean = false;
 
 	constructor() {
@@ -35,7 +35,7 @@ class AppEntry {
 		Globals.init(this.game);
 		Materials.init(this.game);
 		this.game.input.gamepad.start();
-		
+
 
 		this.players.push(new Player(this.game, 1, this.game.input.gamepad.pad1, this.game.input.gamepad.pad2));
 		this.players.push(new Player(this.game, 2, this.game.input.gamepad.pad2, this.game.input.gamepad.pad1));
@@ -43,7 +43,7 @@ class AppEntry {
 		this.sacrificePits.push(new SacrificePit(this.game, this.players[0]));
 		this.sacrificePits.push(new SacrificePit(this.game, this.players[1]));
 
-		
+
 		Globals.gameObjects.push(this.players[0], this.players[1], this.sacrificePits[0], this.sacrificePits[1]);
 
 		for (let i = 0; i < 100; i++) {
@@ -53,6 +53,7 @@ class AppEntry {
 		this.ui = new Ui(this.game, this.players);
 	}
 
+	lastSpawn = 0;
 	update() {
 
 		for (let i = 0; i < Globals.gameObjects.length; i++) {
@@ -61,19 +62,19 @@ class AppEntry {
 				Globals.gameObjects.splice(i, 1);
 				continue;
 			}
-			
+
 			Globals.gameObjects[i].update();
 		}
 		for (let i = Globals.shots.length - 1; i >= 0; i--) {
 			Globals.shots[i].update();
 		}
-		
-		
+
+
 		this.ui.update();
 		//debugger;
 		//this.game.time.physicsElapsed
 		
-		if (this.game.input.gamepad.pad1.connected) {			
+		if (this.game.input.gamepad.pad1.connected) {
 			if (this.game.input.gamepad.pad1.getButton(Phaser.Gamepad.XBOX360_BACK).isDown) {
 				if (!this.swapButtonDown) {
 					this.swapButtonDown = true;
@@ -82,6 +83,16 @@ class AppEntry {
 				}
 			} else {
 				this.swapButtonDown = false;
+			}
+		}
+
+		if (this.lastSpawn < this.game.time.now - 2000) {
+			this.lastSpawn = this.game.time.now;
+
+			for (let i = 0; i < 1; i++) {
+				Globals.gameObjects.push(new Lamb(this.game, 1280 / 2 - 100 + 200 * Math.random(), 0 + 200 * Math.random()));
+
+				Globals.gameObjects.push(new Lamb(this.game, 1280 / 2 - 100 + 200 * Math.random(), 720 - 200 + 200 * Math.random()));
 			}
 		}
 	}
