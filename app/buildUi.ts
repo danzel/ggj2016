c39561876ff08c3c69b7f5a549aee2a2660410c6
danzel <dave@ironshod.co.nz>
2016-01-30 21:40:20 +1300

    Text on build ui for what is selected

--- app/buildUi.ts
+++ app/buildUi.ts
@@ -6,11 +6,13 @@ class BuildUi {
 	itemSprites: Array<Phaser.Sprite> = [];
 	selectorSprite: Phaser.Sprite;
 	visible: boolean = false;
-	
+
+	text: Phaser.Text;
+
 	constructor(game: Phaser.Game, private player: Player) {
 		this.game = game;
 		this.group = game.add.group(null);
-		var spriteNames = [ 'harvester', 'spam', 'warrior', 'tank', 'flying', 'thrower' ];
+		var spriteNames = ['harvester', 'spam', 'warrior', 'tank', 'flying', 'thrower'];
 		this.selectorSprite = game.add.sprite(0, 0, 'selector' + player.id, undefined, this.group);
 		this.selectorSprite.anchor.x = 0.5;
 		this.selectorSprite.anchor.y = 0.35;
@@ -21,10 +23,27 @@ class BuildUi {
 			sprite.anchor.x = 0.5;
 			sprite.anchor.y = 0.5;
 			sprite.alpha = 0.65;
+			sprite.scale.set(0.5);
 			this.itemSprites.push(sprite);
 		}
+
+		this.text = new Phaser.Text(game, -100, -110, "Shooter", {
+			boundsAlignH: 'center',
+			stroke: '#ffffff',
+			strokeThickness: 4
+		});
+		this.text.setTextBounds(0, 0, 200, 50);
+		this.group.add(this.text);
+		
+		/*if (this.player.id == 1) {
+			this.group.x = 130;
+			this.group.y = 720 - 130;
+		} else {
+			this.group.x = 1280 - 130;
+			this.group.y = 720 - 130;
+		}*/
 	}
-	
+
 	update() {
 		var visible = this.player.timeBuildVisible < 1;
 		if (visible) {
@@ -33,6 +52,8 @@ class BuildUi {
 			var v = this.getPosition(this.player.selectedItem);
 			this.selectorSprite.x = v.x;
 			this.selectorSprite.y = v.y;
+
+			this.text.setText(this.getText(this.player.selectedItem));
 		}
 		if (visible == this.visible)
 			return;
@@ -42,12 +63,29 @@ class BuildUi {
 		else
 			this.game.world.remove(this.group);
 	}
-	
+
+	private getText(i: number) {
+		switch (i) {
+			case 0:
+				return "Harvester";
+			case 1:
+				return "Shoal";
+			case 2:
+				return "Warrior";
+			case 3:
+				return "Tank";
+			case 4:
+				return "Flyer";
+			case 5:
+				return "Thrower";
+		}
+	}
+
 	private getPosition(i: number) {
-		var angle = i / 6 * (Math.PI*2) - Math.PI/2;
-		var x = Math.cos(angle) * 110;
-		var y = Math.sin(angle) * 110;
-		return { x: x, y: y };		
+		var angle = i / 6 * (Math.PI * 2) - Math.PI / 2;
+		var x = Math.cos(angle) * 60;
+		var y = Math.sin(angle) * 60;
+		return { x: x, y: y };
 	}
 }
 
